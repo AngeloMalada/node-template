a94b905e39764c8d5afd71a763e02a7ea46acca7
Angelo Malada <64404406+AngeloMalada@users.noreply.github.com>
2023-02-03 09:33:40 +0100

    created project

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    printWidth: 80,
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    trailingComma: "all",
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+};
